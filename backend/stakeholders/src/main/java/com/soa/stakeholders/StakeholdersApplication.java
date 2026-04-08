c4656a5034e28f6d3e554ba5ba23be4458bd1066
aurenaa <salapuranev@gmail.com>
2026-04-08 15:42:12 +0200

    initial commit

---
+++ backend/stakeholders/src/main/java/com/soa/stakeholders/StakeholdersApplication.java
@@ -0,0 +1,13 @@
+package com.soa.stakeholders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StakeholdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StakeholdersApplication.class, args);
+	}
+
+}
---
+++ backend/stakeholders/src/test/java/com/soa/stakeholders/StakeholdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soa.stakeholders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StakeholdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
