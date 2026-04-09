42679db381b2ddfeaaf2e50dcfbb4255ba3328d5
aurenaa <salapuranev@gmail.com>
2026-04-09 11:56:12 +0200

    ui: added profile component with html and css

--- frontend/stakeholders/src/app/app-routing.module.ts
+++ frontend/stakeholders/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
