13d4fbfc18ca462787f67c4e9726bd40d79a2c2d
aurenaa <salapuranev@gmail.com>
2026-04-09 10:00:04 +0200

    ui: added registration component and changed html of login

--- frontend/stakeholders/src/app/register/register.component.ts
+++ frontend/stakeholders/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  userData = {
+    username: '',
+    email: '',
+    password: '',
+    firstname: '',
+    lastname: '',
+    roleName: 'TOURIST'
+  };
 
+  confirmPassword = '';
+  submitted = false;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit(registerForm: NgForm) : void {
+    this.submitted = true;
+
+    if (registerForm.invalid || this.userData.password !== this.confirmPassword) {
+      return;
+    }
+
+    this.authService.register(this.userData).subscribe({
+      next: () => {
+        this.router.navigate(['/login']), { queryParams: { registered: 'true' } }
+      },
+      error: (error) => {
+
+      }
+    })
+
+  }
 }
