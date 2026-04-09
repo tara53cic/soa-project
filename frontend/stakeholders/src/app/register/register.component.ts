12ab751883fd2bcdc62413db154c4c0305ce0087
aurenaa <salapuranev@gmail.com>
2026-04-09 10:53:20 +0200

    fix: fixed first name and last name not saving to data base

--- frontend/stakeholders/src/app/register/register.component.ts
+++ frontend/stakeholders/src/app/register/register.component.ts
@@ -13,8 +13,8 @@ export class RegisterComponent {
     username: '',
     email: '',
     password: '',
-    firstname: '',
-    lastname: '',
+    firstName: '',
+    lastName: '',
     roleName: 'TOURIST'
   };
 
