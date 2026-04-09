f30002cd64285381ae43d6018550a399d55f1d44
Pavicevic Tea <pavicevictea6@gmail.com>
2026-04-09 14:37:07 +0200

    feat: added repository

--- backend/blog/BlogService/Program.cs
+++ backend/blog/BlogService/Program.cs
@@ -1,4 +1,5 @@
 using BlogService.Data;
+using BlogService.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<BlogDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<BlogRepository>();
 
 builder.Services.AddCors(opt =>
     opt.AddPolicy("AllowAll", p =>
---
+++ backend/blog/BlogService/Repositories/BlogRepository.cs
@@ -0,0 +1,31 @@
+﻿using BlogService.Data;
+using BlogService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogService.Repositories;
+
+public class BlogRepository
+{
+    private readonly BlogDbContext _db;
+
+    public BlogRepository(BlogDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<List<Blog>> GetAllAsync()
+    {
+        return await _db.Blogs
+            .Include(b => b.Images)
+            .ToListAsync();
+    }
+
+    public async Task<Blog?> GetByIdAsync(Guid id)
+    {
+        var blog = await _db.Blogs
+            .Include(b => b.Images)
+            .FirstOrDefaultAsync(b => b.Id == id);
+
+        return blog;
+    }
+}
