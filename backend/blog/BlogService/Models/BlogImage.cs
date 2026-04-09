b232c30b97b262a25d423939fc34a6ece32ac4db
Pavicevic Tea <pavicevictea6@gmail.com>
2026-04-09 13:12:00 +0200

    feat: domain models added

---
+++ backend/blog/BlogService/Models/BlogImage.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogService.Models;
+
+public class BlogImage
+{
+    public Guid Id { get; set; }
+    public string Url { get; set; } = string.Empty;
+    public Guid BlogId { get; set; }
+    public Blog Blog { get; set; } = null!;
+}
