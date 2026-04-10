7b2e7c584edd60d503c93b51f2e99500d74002e3
Pavicevic Tea <pavicevictea6@gmail.com>
2026-04-10 02:23:54 +0200

    ui: blog list with horizontal scroll and see all view implemented

--- frontend/stakeholders/src/app/app-routing.module.ts
+++ frontend/stakeholders/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { BlogCreateComponent } from './blog/blog-create/blog-create.component';
+import { BlogListComponent } from './blog/blog-list/blog-list.component';
+import { BlogDetailComponent } from './blog/blog-detail/blog-detail.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'create-blog', component: BlogCreateComponent },
+  { path: 'blogs', component: BlogListComponent },
+  { path: 'blogs/:id', component: BlogDetailComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
--- frontend/stakeholders/src/app/blog/blog-list/blog-list.component.ts
+++ frontend/stakeholders/src/app/blog/blog-list/blog-list.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Blog } from '../../models/blog.model';
+import { BlogService } from '../../services/blog.service';
 
 @Component({
   selector: 'app-blog-list',
   templateUrl: './blog-list.component.html',
   styleUrls: ['./blog-list.component.css']
 })
-export class BlogListComponent {
+export class BlogListComponent implements OnInit {
+  @Input() limit: number = 0;
+  
+  blogs: Blog[] = [];
+  displayBlogs: Blog[] = [];
+  isLoading = true;
+  error = '';
 
+  constructor(private blogService: BlogService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.blogService.getAll().subscribe({
+      next: (data) => { 
+        this.blogs = data;
+        this.displayBlogs = (this.limit > 0) ? this.blogs.slice(0, this.limit) : this.blogs;
+        this.isLoading = false; 
+      },
+      error: () => { 
+        this.error = 'Error loading blogs.'; 
+        this.isLoading = false; 
+      }
+    });
+  }
+
+  goToSeeAll(): void {
+    this.router.navigate(['/blogs']);
+  }
+
+  formatDate(dateStr: string): string {
+    return new Date(dateStr).toLocaleDateString('en-US', {
+      day: 'numeric', month: 'short', year: 'numeric'
+    });
+  }
+
+  getInitials(username: string): string {
+    return username ? username.substring(0, 2).toUpperCase() : 'AU';
+  }
+
+  goBack(): void {
+    this.router.navigate(['/home']);
+  }
+
+  goToBlog(id: string): void {
+    this.router.navigate(['/blogs', id]);
+  }
 }
