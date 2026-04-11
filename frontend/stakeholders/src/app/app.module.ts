1813e212941a0c3341d80f406b931c6c39ded63e
Pavicevic Tea <pavicevictea6@gmail.com>
2026-04-12 00:35:00 +0200

    ui: edit component and routing for edit implemented

--- frontend/stakeholders/src/app/app.module.ts
+++ frontend/stakeholders/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BlogDetailComponent } from './blog/blog-detail/blog-detail.component';
 import { ProfileComponent } from './profile/profile.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.compo
     ProfileComponent,
     AdminDashboardComponent,
     UserDetailsComponent,
+    EditProfileComponent,
   ],
   imports: [
     BrowserModule,
