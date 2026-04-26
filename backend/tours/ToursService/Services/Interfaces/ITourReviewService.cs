fe2dd1483a6ca1e06cabf541900282555ff19cbe
Pavicevic Tea <pavicevictea6@gmail.com>
2026-04-26 17:52:36 +0200

    feat: service class for reveiws implemented

---
+++ backend/tours/ToursService/Services/Interfaces/ITourReviewService.cs
@@ -0,0 +1,10 @@
+﻿using ToursService.DTOs;
+
+namespace ToursService.Services.Interfaces
+{
+    public interface ITourReviewService
+    {
+        TourReviewDto CreateReview(TourReviewDto reviewDto);
+        List<TourReviewDto> GetByTourId(long tourId);
+    }
+}
---
+++ backend/tours/ToursService/Services/TourReviewService.cs
@@ -0,0 +1,53 @@
+﻿using ToursService.DTOs;
+using ToursService.Models;
+using ToursService.Repositories.Interfaces;
+using ToursService.Services.Interfaces;
+
+namespace ToursService.Services
+{
+    public class TourReviewService : ITourReviewService
+    {
+        private readonly ITourReviewRepository _tourReviewRepository;
+
+        public TourReviewService(ITourReviewRepository reviewRepository)
+        {
+            _tourReviewRepository = reviewRepository;
+        }
+
+        public TourReviewDto CreateReview(TourReviewDto reviewDto)
+        {
+            var review = new TourReview(
+                reviewDto.Grade,
+                reviewDto.Comment,
+                reviewDto.TouristId,
+                reviewDto.TourId,
+                reviewDto.AttendanceDate,
+                reviewDto.Images
+            );
+
+            var savedReview = _tourReviewRepository.CreateReview(review);
+            return MapToDto(savedReview);
+        }
+
+        public List<TourReviewDto> GetByTourId(long tourId)
+        {
+            var reviews = _tourReviewRepository.GetByTourId(tourId);
+            return reviews.Select(MapToDto).ToList();
+        }
+
+        private TourReviewDto MapToDto(TourReview review)
+        {
+            return new TourReviewDto
+            {
+                Id = review.Id,
+                Grade = review.Grade,
+                Comment = review.Comment,
+                TouristId = review.TouristId,
+                AttendanceDate = review.AttendanceDate,
+                ReviewDate = review.ReviewDate,
+                Images = review.Images,
+                TourId = review.TourId
+            };
+        }
+    }
+}
