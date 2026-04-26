676f83e2596f4bea6447838ee3d1a7ad35428433
Pavicevic Tea <pavicevictea6@gmail.com>
2026-04-26 16:49:00 +0200

    feat: repository implemented

---
+++ backend/tours/ToursService/Repositories/TourReviewRepository.cs
@@ -0,0 +1,28 @@
+﻿using ToursService.Data;
+using ToursService.Models;
+using ToursService.Repositories.Interfaces;
+
+namespace ToursService.Repositories
+{
+    public class TourReviewRepository : ITourReviewRepository
+    {
+        private readonly ToursDbContext _context;
+
+        public TourReviewRepository(ToursDbContext context)
+        {
+            _context = context;
+        }
+
+        public TourReview CreateReview(TourReview review)
+        {
+            _context.TourReviews.Add(review);
+            _context.SaveChanges();
+            return review;
+        }
+
+        public List<TourReview> getByTourId(long tourId)
+        {
+            return _context.TourReviews.Where(r => r.TourId == tourId).ToList();
+        }
+    }
+}
