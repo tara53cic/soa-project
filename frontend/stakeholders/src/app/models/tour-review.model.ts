4dd1e58bcd06636ff4913f7bd392a7b90d5bed36
Pavicevic Tea <pavicevictea6@gmail.com>
2026-04-26 23:19:35 +0200

    ui: tour review model and service methods implemented

---
+++ frontend/stakeholders/src/app/models/tour-review.model.ts
@@ -0,0 +1,11 @@
+export interface TourReview {
+    id?: number;
+    grade: number;
+    comment: string;
+    touristId: number;
+    tourId: number;
+    attendanceDate: string;
+    reviewDate?: string;
+    images?: string[];
+    imageFiles?: File[];
+}
