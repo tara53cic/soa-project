f2d55a2f79e35c3c4bb8db20f3e3fb80aeb27859
Pavicevic Tea <pavicevictea6@gmail.com>
2026-04-26 02:06:02 +0200

    feat: dto class added

---
+++ backend/tours/ToursService/DTOs/TourReviewDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ToursService.DTOs
+{
+    public class TourReviewDto
+    {
+        public long Id { get; set; }
+        public int Grade { get; set; }
+        public string Comment { get; set; }
+        public long TouristId { get; set; }
+        public long TourId { get; set; }
+        public DateTime AttendanceDate { get; set; }
+        public DateTime ReviewDate { get; set; }
+        public List<string>? Images { get; set; }
+    }
+}
