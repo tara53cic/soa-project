8c735b598c11285d5536e690b732c5433f98b751
Pavicevic Tea <pavicevictea6@gmail.com>
2026-05-07 01:34:51 +0200

    feat: BlogDbContext replaced with MongoDbContext

---
+++ backend/blog/BlogService/Data/MongoDbContext.cs
@@ -0,0 +1,35 @@
+﻿using BlogService.Models;
+using MongoDB.Driver;
+
+namespace BlogService.Data
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration["MongoDbSettings:ConnectionString"]);
+            _database = client.GetDatabase(configuration["MongoDbSettings:DatabaseName"]);
+
+            ConfigureIndexes();
+        }
+
+        public IMongoCollection<Blog> Blogs => _database.GetCollection<Blog>("Blogs");
+
+        private void ConfigureIndexes()
+        {
+            var likeIndexModel = new CreateIndexModel<Blog>(
+                Builders<Blog>.IndexKeys.Ascending("Likes.Username")
+                    .Ascending(b => b.Id),
+                new CreateIndexOptions { Unique = true, Sparse = true }
+            );
+
+            var authorIndexModel = new CreateIndexModel<Blog>(
+                Builders<Blog>.IndexKeys.Ascending(b => b.AuthorUsername)
+            );
+
+            Blogs.Indexes.CreateMany(new[] { likeIndexModel, authorIndexModel });
+        }
+    }
+}
